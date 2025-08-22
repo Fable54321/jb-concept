5bcc0d75a98387a2da6e9ebf6222d228b188d193
Timothe Bissonnette <timothe5@hotmail.com>
2025-08-22 18:05:52 -0400

    first commit

--- src/App/Root/Home/Home.tsx
+++ src/App/Root/Home/Home.tsx
@@ -1,81 +1,85 @@
-import styles from './Home.module.scss'
-import image1 from '../../../assets/photo--1.png'
-import image2 from '../../../assets/photo--2.png'
-import image3  from '../../../assets/image--3.jpg'
-import HomePictureGrid from './HomePictureGrid/HomePictureGrid'
-import { useState } from 'react'
-import HomeAdditional from './HomeAdditional/HomeAdditional'
+import styles from "./Home.module.scss";
+import image1 from "../../../assets/photo--1.png";
+import image2 from "../../../assets/photo--2.png";
+import image3 from "../../../assets/image--3.jpg";
+import HomePictureGrid from "./HomePictureGrid/HomePictureGrid";
+import { useState } from "react";
+import HomeAdditional from "./HomeAdditional/HomeAdditional";
 
 const Home = () => {
+  const [position, setPosition] = useState(1);
+  const [opacity, setOpacity] = useState(1);
 
-const [position, setPosition] = useState(1);
-const [opacity, setOpacity] = useState(1);
-
-const switching = (page : number) => {
+  const switching = (page: number) => {
     setOpacity(0);
     setTranslateX(-50);
 
     setTimeout(() => {
-        setPosition(page)
-        setOpacity(1);
-        setTranslateX(0);
-    }, 500)
+      setPosition(page);
+      setOpacity(1);
+      setTranslateX(0);
+    }, 500);
+  };
 
-    
-}
+  const srcs = [image1, image2, image3];
+  const texts1 = ["Le bonheur", "Faites la différence ", "Des uniformes"];
+  const texts2 = ["se porte tous ", "avec des cadeaux", "éco-chics pour"];
 
-const srcs = [
-    image1,
-    image2,
-    image3]
-const texts1 = [
-    "Le bonheur",
-    "Faites la différence ",
-    "Des uniformes"  
-]
-const texts2 =[
-    "se porte tous ",
-    "avec des cadeaux",
-    "éco-chics pour"
-]   
+  const texts3 = ["les jours !", "mémorables.", "un avenir plus vert !"];
 
-const texts3 =[
-    "les jours !",
-    "mémorables.",
-    "un avenir plus vert !" 
-]
-
-const [translateX, setTranslateX] = useState(0);
- 
+  const [translateX, setTranslateX] = useState(0);
 
   return (
     <section className={styles["home"]}>
-        <h2 className={styles["sr-only"]}>Page d'accueil</h2>
-        <article className={styles['home__carousel']}>
-            <div className={styles['home__carousel__banner']}></div>
-                <img src={srcs[position-1]} alt="" className={styles['home__carousel__image']} style={{opacity: opacity, transform: `translateX(${translateX}%)`}} />
-            <div className={styles['home__carousel__text--wrapper']}>
-                <p className={`${styles['home__carousel__text']} ${styles['home__carousel__text--1']}`} style={{opacity: opacity}} >{texts1[position-1]}</p>
-                <p className={`${styles['home__carousel__text']} ${styles['home__carousel__text--2']}`} style={{opacity: opacity}} >{texts2[position-1]}</p>
-                <p className={`${styles['home__carousel__text']} ${styles['home__carousel__text--3']}`} style={{opacity: opacity}} >{texts3[position-1]}</p>
-                
-            </div>
-            
-        </article>
-        <div className={styles['home__carousel__button--wrapper']}>
-            {[1,2,3].map(n => (
-                <button key={n} onClick={() => {
-                    setTimeout(() => switching(n))
-                }} style={{background: position === n ? '#118ab2' : 'white'}} className={styles['home__carousel__button']}>
-                    
-                </button>
-            ))}
+      <h2 className={styles["sr-only"]}>Page d'accueil</h2>
+      <article className={styles["home__carousel"]}>
+        <div className={styles["home__carousel__banner"]}></div>
+        <img
+          src={srcs[position - 1]}
+          alt=""
+          className={styles["home__carousel__image"]}
+          style={{ opacity: opacity, transform: `translateX(${translateX}%)` }}
+        />
+        <div className={styles["home__carousel__text--wrapper"]}>
+          <p
+            className={`${styles["home__carousel__text"]} ${styles["home__carousel__text--1"]}`}
+            style={{ opacity: opacity }}
+          >
+            {texts1[position - 1]}
+          </p>
+          <p
+            className={`${styles["home__carousel__text"]} ${styles["home__carousel__text--2"]}`}
+            style={{ opacity: opacity }}
+          >
+            {texts2[position - 1]}
+          </p>
+          <p
+            className={`${styles["home__carousel__text"]} ${styles["home__carousel__text--3"]}`}
+            style={{ opacity: opacity }}
+          >
+            {texts3[position - 1]}
+          </p>
         </div>
-        <h2 className={styles["home__title"]}	>Votre solution pour des vêtements personalisés de qualité </h2>
-        <HomePictureGrid />
-        <HomeAdditional />
+      </article>
+      <div className={styles["home__carousel__button--wrapper"]}>
+        {[1, 2, 3].map((n) => (
+          <button
+            key={n}
+            onClick={() => {
+              setTimeout(() => switching(n));
+            }}
+            style={{ background: position === n ? "#118ab2" : "white" }}
+            className={styles["home__carousel__button"]}
+          ></button>
+        ))}
+      </div>
+      <h2 className={styles["home__title"]}>
+        Votre solution pour des vêtements personnalisés de qualité{" "}
+      </h2>
+      <HomePictureGrid />
+      <HomeAdditional />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
